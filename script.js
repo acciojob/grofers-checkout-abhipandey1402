31d0a1de808079d965613692e2ee62c4614f664c
Abhi Pandey <97884726+abhipandey1402@users.noreply.github.com>
2023-04-19 18:15:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,14 @@
-const getSumBtn = document.createElement("button");
-getSumBtn.append("Get Total Price");
-document.body.appendChild(getSumBtn);
+var table = document.querySelector('table');
+ var lastRow = document.createElement('tr');
+ var prices = document.querySelectorAll('[data-ns-test=price]');
+ let sum = 0;
+ for(let i = 0; i < prices.length; i++) {
+  sum += parseInt(prices[i].textContent);
+ }
+ var child = document.createElement("td");
+ child.setAttribute('data-ns-test', 'grandTotal');
+ 
 
-const getSum = () => {
-//Add your code here
-	// Get all the price elements from the table
-const priceElements = document.querySelectorAll('.price');
-
-// Convert the NodeList into an array and map the prices to numbers
-const prices = Array.from(priceElements).map(priceElement => parseFloat(priceElement.textContent));
-
-// Calculate the total price by adding up all the prices
-const totalPrice = prices.reduce((total, price) => total + price, 0);
-
-// Create a new row for the total price
-const newRow = document.createElement('tr');
-
-// Create a cell for the total price and set its colspan to 2
-const totalCell = document.createElement('td');
-totalCell.setAttribute('colspan', '2');
-
-// Set the text content of the total cell to the total price
-totalCell.textContent = `Total: Rs ${totalPrice}`;
-
-// Append the total cell to the new row
-newRow.appendChild(totalCell);
-
-// Append the new row to the table
-const table = document.querySelector('table');
-table.appendChild(newRow);
-};
-
-getSumBtn.addEventListener("click", getSum);
-
+ child.textContent = sum;
+ lastRow.appendChild(child);
+ table.appendChild(lastRow);
